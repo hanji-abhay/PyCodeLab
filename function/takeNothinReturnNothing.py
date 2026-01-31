daf2c5d9469bba5b55267e1d7f86c249a1e15f3b
Abhay Mathur <abhaymathur629@gmail.com>
2026-01-31 20:03:09 +0530

    Create takeNothinReturnNothing.py

---
+++ function/takeNothinReturnNothing.py
@@ -0,0 +1,9 @@
+def greater():
+    print("Enter Two number:")
+    x= eval(input())
+    y=eval(input()) 
+    if x>y:
+        print("x is greater")
+    else:
+       print("y is greater")
+greater()
