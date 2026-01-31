d53c67466ff7e380240b3fa800e4c503c5046396
Abhay Mathur <abhaymathur629@gmail.com>
2026-01-31 20:14:08 +0530

    Add inheritance example with basic arithmetic operations

---
+++ oop/inheritance_example.py
@@ -0,0 +1,18 @@
+class A:
+      def add(self):
+            a = int(input("enter num: "))
+            b = int(input("enter num: "))
+            print(a+b)
+      def sub(self):
+            a = int(input("enter num: "))
+            b = int(input("enter num: "))
+            print(a-b)
+class manage(A):
+      def b(self):
+            a = int(input("enter num: "))
+            b = int(input("enter num: "))
+            print(a*b)
+m = manage()
+m.add()
+m.sub()
+m.b()
