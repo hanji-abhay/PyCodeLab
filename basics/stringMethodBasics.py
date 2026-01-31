d4da0ccda9ec24451ac73e02c40e8ba7c898c123
Abhay Mathur <abhaymathur629@gmail.com>
2026-01-31 19:08:48 +0530

    Create stringMethodBasics.py

---
+++ basics/stringMethodBasics.py
@@ -0,0 +1,14 @@
+a = '''
+i am abhay.
+i want to be an ai/ml developer
+'''
+c =  a.capitalize()   #capitalizes the string
+print(c)
+b = a.title()         #converts the string in (Title Format)
+print(b)
+u = a.upper()         #converts string in upper case
+d = a.isupper()       #returns true or false when string is in upper case or not
+print(d)
+l = u.lower()         #converts string in lower case
+d = l.islower()       #returns true or false when string is in lower case or not
+print(d)
